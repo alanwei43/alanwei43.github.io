c887a585b9d21c5ef802c5563292742d7b1e2421
Alan <alan@Alans-MacBook-Pro.local>
2021-03-06 22:42:36 +0800

    Deploy website - based on b6dc9faa71f62cc6fd86b637b49d0d04df468211

---
+++ resources/scripts/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6340dc5f9c3028a3534a752c6fea87b7";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
