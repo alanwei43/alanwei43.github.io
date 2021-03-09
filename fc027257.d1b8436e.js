4d9bd6f6dbbbcc2576332659bb8230c6ed47ff0d
Alan <alan.wei@live.com>
2021-03-09 20:58:54 +0800

    update

---
+++ fc027257.d1b8436e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[131],{201:function(u){u.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"ubuntu","name":"ubuntu","count":1,"permalink":"/blog/tags/ubuntu"}')}}]);
