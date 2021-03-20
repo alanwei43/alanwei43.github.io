e96628edfc24f9adaa2fd3aaac8455267afce803
Alan <alan.wei@live.com>
2021-03-20 15:10:16 +0800

    update

--- resources/scripts/baidu.js
+++ resources/scripts/baidu.js
@@ -1,7 +1,11 @@
 var _hmt = _hmt || [];
-(function() {
+(function () {
+  if (location.hostname === "localhost") {
+    console.log("ignore baidu hm.js");
+    return;
+  }
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?01f1201db9004ef39e671e84ac4e66ec";
-  var s = document.getElementsByTagName("script")[0]; 
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
