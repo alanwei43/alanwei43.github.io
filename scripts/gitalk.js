d5f258d8c8108ee0b8319145a2a496cddfaf4bd3
gh-actions <actions@github.com>
2022-04-07 14:59:09 +0000

    Deploy website - based on 8d73efd9523d463233d2048026d081b6c0fa464e

---
+++ scripts/gitalk.js
@@ -0,0 +1,25 @@
+(function () {
+  const script = document.createElement("script");
+  script.src = "/scripts/gitalk.min.js";
+  script.defer = true;
+  script.async = true;
+  script.addEventListener("load", function () { 
+    const gitalk = new Gitalk({
+      clientID: '23c4db407bb2651297c5',
+      clientSecret: 'fc3130cc8193193e948f4230a1ee5ebdf17ae55f',
+      repo: 'alanwei43.github.io',      // The repository of store comments,
+      owner: 'alanwei43',
+      admin: ['alanwei43'],
+      id: location.pathname,      // Ensure uniqueness and length less than 50
+      distractionFreeMode: false  // Facebook-like distraction free mode
+    });
+    
+    gitalk.render('gitalk-container')
+  });
+  const style = document.createElement("link");
+  style.rel = "stylesheet";
+  style.href = "/styles/gitalk.css";
+
+  document.head.appendChild(script);
+  document.head.appendChild(style);
+})();
