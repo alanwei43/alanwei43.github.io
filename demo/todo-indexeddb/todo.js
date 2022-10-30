0a3920bf6d187a881309f75d6bbf4847e0223278
gh-actions <actions@github.com>
2022-10-30 13:10:10 +0000

    Deploy website - based on 84bc4f23001ac50a6e5647fa54979a3d51e9c09d

--- demo/todo-indexeddb/todo.js
+++ demo/todo-indexeddb/todo.js
@@ -2,19 +2,23 @@ import { indexedDbUtils } from "./indexedDbUtils.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   indexedDbUtils({
-    dbVersion: 2,
+    dbVersion: 3,
     dbName: "db-todo",
     storeName: "store-todo"
   }).then(utils => {
     document.querySelector("#form")
       .addEventListener("submit", e => {
         e.preventDefault();
+        const content = document.querySelector("input[name='todo.content']");
         utils.addItem({
           key: generateKey(),
-          content: document.querySelector("input[name='todo.content']").value,
-          addDate: new Date().toISOString(),
+          content: content.value,
+          addDate: new Date().toLocaleString(),
           finishDate: null,
-        }).then(() => listAllTodo(utils));
+        }).then(() => {
+          listAllTodo(utils);
+          content.value = "";
+        });
       });
 
     listAllTodo(utils);
@@ -43,10 +47,20 @@ document.addEventListener("DOMContentLoaded", () => {
 function listAllTodo(utils) {
   utils.listAll().then(data => {
     document.querySelector("#todo-list").innerHTML = data
+      .sort((prev, next) => {
+        if (prev.raw.finishDate && !next.raw.finishDate) {
+          return 1;
+        }
+        if (!prev.raw.finishDate && next.raw.finishDate) {
+          return -1;
+        }
+        return next.key - prev.key;
+      })
       .map(item => `
-<li>
+<li class="${item.raw.finishDate ? 'done' : ''}">
   <input type="checkbox" ${item.raw.finishDate ? "checked" : ""} data-todo='${JSON.stringify(item)}' />
   <span>${item.raw.content}</span>
+  <i>${item.raw.addDate}</i>
   <button data-todo='${JSON.stringify(item)}'>Delete</button>
 </li>
 `).join("");
