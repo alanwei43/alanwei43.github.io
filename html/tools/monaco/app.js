972af778b4e93a76beef058cc05171ec95b94620
gh-actions <actions@github.com>
2024-05-08 15:00:09 +0000

    Deploy website - based on 873ab7dcf04e03f052634bba508bc917d6583c21

--- html/tools/monaco/app.js
+++ html/tools/monaco/app.js
@@ -1,19 +1,25 @@
 (function () {
   require.config({ paths: { vs: './monaco-editor/min/vs' } });
   const source = "monaco-editor";
+
   const params = new URLSearchParams(location.search);
+  const options = JSON.parse(params.get("create") || "{}");
+  const isDebug = params.get("debug") === "1";
 
   /**
    * @type {Promise<monaco.editor.IStandaloneCodeEditor>}
    */
   const monaco_editor = new Promise(resolve => {
+    isDebug && console.log(`接收到初始化参数: `, options);
     require(['vs/editor/editor.main'], function () {
       const container = document.getElementById('container');
-      const options = JSON.parse(params.get("create") || "{}");
+      isDebug && console.log(`开始初始化`);
       const editor = monaco.editor.create(container, options);
       resolve(editor);
+      isDebug && console.log(`初始化完成`);
 
       editor.onDidChangeModelContent(function () {
+        isDebug && console.log(`编辑器内容发生变化, 最新内容为: \n`, editor.getValue());
         window.parent.postMessage({
           source: source,
           action: "change",
@@ -36,7 +42,7 @@
 
   window.addEventListener("message", function (e) {
     const data = e.data;
-    console.log("child receive message event: ", data);
+    isDebug && console.log("child receive message event: ", data);
     if (data && data.source === source && data.action === "update") {
       monaco_editor.then(editor => {
         editor.setValue(data.payload);
